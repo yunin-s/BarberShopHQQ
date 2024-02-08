b7e81f96686e9e7129fd4d8d4faadd668c562328
yunin-s <yunin2876@mail.ru>
2024-02-08 09:59:19 +0700

    install gem rake

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
